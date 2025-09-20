1b70cd4993146f721b77d0f7f493fa714217448d
Lamaro25 <Customdesignsla@icloud.com>
2025-09-20 00:04:36 -0500

    Fix auth.js: correct oauthProvider import and usage

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,13 +1,12 @@
 // netlify/functions/auth.js
-const { default: oauthProvider } = require("netlify-cms-oauth-provider-node");
+const oauthProvider = require("netlify-cms-oauth-provider-node");
 
 exports.handler = oauthProvider({
-  // use your Auth0 details here
-  provider: "auth0",
+  // pulled from your Netlify environment variables
   client_id: process.env.AUTH0_CLIENT_ID,
   client_secret: process.env.AUTH0_CLIENT_SECRET,
+
+  // use your Auth0 tenant URLs
   auth_url: "https://dev-i1asef6df0034r3d.us.auth0.com",
   token_url: "https://dev-i1asef6df0034r3d.us.auth0.com/oauth/token",
-  // match your API identifier
-  audience: "https://api.customdesignsla.com",
 });
