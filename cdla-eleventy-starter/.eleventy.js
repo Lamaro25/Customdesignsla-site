2caea1a6e5acb461e894d5364c8bf88df304c233
Lamaro25 <Customdesignsla@icloud.com>
2025-09-30 23:09:03 -0500

    fix: point Eleventy includes/layouts to content/_includes

--- cdla-eleventy-starter/.eleventy.js
+++ cdla-eleventy-starter/.eleventy.js
@@ -1,11 +1,14 @@
 module.exports = function(eleventyConfig) {
+  // Copy assets directly to the output folder
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("static");
 
   return {
     dir: {
-      input: "content",
-      output: "_site"
+      input: "content",       // all your markdown/products live here
+      includes: "_includes",  // 👈 tells Eleventy to look in content/_includes
+      layouts: "_includes",   // 👈 layouts also live in content/_includes
+      output: "_site"         // Netlify serves this folder
     },
     templateFormats: ["md", "njk", "html"],
     markdownTemplateEngine: "njk",
