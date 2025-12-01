d0dce635b2db08f90264b0355e16da76193a857b
Lamaro25 <Customdesignsla@icloud.com>
2025-12-01 00:08:28 -0600

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -41,9 +41,9 @@ module.exports = function (eleventyConfig) {
   // The tag "cowboy_hat_picks" is used in the frontmatter
   // This guarantees Eleventy registers the collection.
   // ------------------------------
-  eleventyConfig.addCollection("cowboy_hat_picks", function (collectionApi) {
-    return collectionApi.getFilteredByTag("cowboy_hat_picks");
-  });
+  eleventyConfig.addCollection("cowboy_hat_picks", function(collectionApi) {
+  return collectionApi.getFilteredByGlob("content/LTR/cowboy-hat-picks/*.md");
+});
 
   // ------------------------------
   // GLOBAL COMPUTED DATA
